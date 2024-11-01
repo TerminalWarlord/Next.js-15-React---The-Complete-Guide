7758bb7a3da9c269218b10b9641602c85401829f
Joy Biswas <joybiswas389@gmail.com>
2024-11-02 00:19:45 +0600

    added server action response using useActionState() hook

--- module3-nextjs-essentials/components/meals/meals-form-submit.js
+++ module3-nextjs-essentials/components/meals/meals-form-submit.js
@@ -1,29 +1,15 @@
-// 'use client';
-// import { useFormStatus } from 'react-dom';
-
-
-// const MealsFormSubmit = () => {
-//   const { pending } = useFormStatus();
-//   console.log(pending);
-//   return (
-//     <button disabled={pending}>
-//       {pending ? 'Submitting' : 'Share Meal'}
-//     </button>
-//   )
-// }
-
-// export default MealsFormSubmit
-
 'use client';
-
 import { useFormStatus } from 'react-dom';
 
-export default function MealsFormSubmit() {
-  const { pending } = useFormStatus();
 
+const MealsFormSubmit = () => {
+  const { pending } = useFormStatus();
+  console.log(pending);
   return (
     <button disabled={pending}>
-      {pending ? 'Submitting...' : 'Share Meal'}
+      {pending ? 'Submitting' : 'Share Meal'}
     </button>
-  );
+  )
 }
+
+export default MealsFormSubmit
